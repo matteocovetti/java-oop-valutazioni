fb51771332110e7193dd912849c4d14ab904c1b8
matteocovetti <covetti.matteo@gmail.com>
2022-06-16 17:04:04 +0200

    esercizio finito

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -1,5 +1,7 @@
 package jana60.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 	/*
 	 * create un array di studenti (i dati degli studenti possono essere chiesti
@@ -10,7 +12,31 @@ public class Main {
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
+		Studente[] studenti = new Studente[5];
+
+		int studentiPromossi = 0;
+
+		for (int i = 0; i < studenti.length; i++) {
+			int percAssenze = random.nextInt(101);
+			double mediaVoti = random.nextDouble() * 5;
+			int idStudente = i + 1;
+
+			studenti[i] = new Studente(idStudente, percAssenze, mediaVoti);
+
+			if (studenti[i].promosso()) {
+				System.out.println("Lo studente numero " + idStudente + " è stato promosso");
+				studentiPromossi++;
+			} else {
+				System.out.println("Lo studente numero " + idStudente + " è stato bocciato");
+			}
+		}
+
+		if (studentiPromossi > 1) {
+			System.out.println("Sono stati promossi " + studentiPromossi + " studenti");
+		} else {
+			System.out.println("È stato promosso " + studentiPromossi + " studente");
+		}
 
 	}
 
