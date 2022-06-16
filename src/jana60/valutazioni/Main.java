19a9cd29e7aee5916b9d18731e72b1f897fdd9a3
matteocovetti <covetti.matteo@gmail.com>
2022-06-16 16:08:59 +0200

    first commit

---
+++ src/jana60/valutazioni/Main.java
@@ -0,0 +1,17 @@
+package jana60.valutazioni;
+
+public class Main {
+	/*
+	 * create un array di studenti (i dati degli studenti possono essere chiesti
+	 * all’utente oppure inseriti nel codice con dei valori di test). L’id di ogni
+	 * studente deve essere univoco nell’array (possiamo usare un numero
+	 * progressivo). Il programma deve poi stampare a video per ogni studente se è
+	 * stato promosso o bocciato e quanti studenti sono stati promossi in tutto.
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/jana60/valutazioni/Studente.java
@@ -0,0 +1,11 @@
+package jana60.valutazioni;
+
+public class Studente {
+	/*
+	 * id dello studente (intero), percentuale assenze (intero), media voti (numero
+	 * decimale). La classe deve avere un costruttore con parametri e anche un
+	 * metodo che calcola, in base a percentuale delle assenze e media dei voti, se
+	 * lo studente è promosso o bocciato
+	 */
+
+}
